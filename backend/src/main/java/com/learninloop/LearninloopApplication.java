86ef457b2b46c17e85e4fb28196a585b199539d3
Lokesh Matta <iamlokeshofficial@gmail.com>
2025-04-23 18:27:50 +0530

    initial commit and push

---
+++ backend/src/main/java/com/learninloop/LearninloopApplication.java
@@ -0,0 +1,13 @@
+package com.learninloop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearninloopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearninloopApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/learninloop/LearninloopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learninloop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearninloopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
